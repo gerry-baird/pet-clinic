afbf801360d8e95c2605d63b32e778e800027099
gerry <gerry.baird@uk.ibm.com>
2020-11-19 14:11:27 +0000

    Add PetType in with Bootstrap. closes #30

--- pet-clinic-data/src/main/java/com/gerry/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/gerry/petclinic/model/PetType.java
@@ -2,4 +2,12 @@ package com.gerry.petclinic.model;
 
 public class PetType extends BaseEntity{
     private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- pet-clinic-web/src/main/java/com/gerry/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/gerry/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.gerry.petclinic.bootstrap;
 
 import com.gerry.petclinic.model.Owner;
+import com.gerry.petclinic.model.PetType;
 import com.gerry.petclinic.model.Vet;
 import com.gerry.petclinic.services.OwnerService;
+import com.gerry.petclinic.services.PetTypeService;
 import com.gerry.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,15 +14,26 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
     //These will now be autowired, even without the annotation
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
