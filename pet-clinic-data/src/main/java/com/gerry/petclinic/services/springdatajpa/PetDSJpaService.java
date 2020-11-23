a2fe7c2b05d47648576cd9f4b7fbabc060a3fc73
gerry <gerry.baird@uk.ibm.com>
2020-11-23 17:23:03 +0000

    Implemented JPA service for Pet. closes #41

---
+++ pet-clinic-data/src/main/java/com/gerry/petclinic/services/springdatajpa/PetDSJpaService.java
@@ -0,0 +1,51 @@
+package com.gerry.petclinic.services.springdatajpa;
+
+import com.gerry.petclinic.model.Pet;
+import com.gerry.petclinic.repositories.PetRepository;
+import com.gerry.petclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetDSJpaService implements PetService {
+
+    private final PetRepository petRepository;
+
+    public PetDSJpaService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pets::add);
+        return pets;
+    }
+
+    @Override
+    public Pet findByID(Long aLong) {
+        Optional<Pet> optionalPet = petRepository.findById(aLong);
+
+        return optionalPet.orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
