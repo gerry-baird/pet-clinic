cd3c4dbb54721f5dcef903fba84c7fa381462417
gerry <gerry.baird@uk.ibm.com>
2020-11-23 11:24:09 +0000

    implemented visit entity. closes #36

--- pet-clinic-data/src/main/java/com/gerry/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/gerry/petclinic/model/Pet.java
@@ -1,10 +1,9 @@
 package com.gerry.petclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity(name = "pets")
 public class Pet extends BaseEntity{
@@ -22,6 +21,9 @@ public class Pet extends BaseEntity{
 
     @Column(name="name")
     private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -53,5 +55,13 @@ public class Pet extends BaseEntity{
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
