9825add4abd234e0e6176a8a9733d2116cb48dc9
gerry <gerry.baird@uk.ibm.com>
2020-11-25 10:44:49 +0000

    Refactored data project to use Lombok. closes #51

--- pet-clinic-data/src/main/java/com/gerry/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/gerry/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package com.gerry.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name="vets")
 public class Vet extends Person{
@@ -14,11 +21,5 @@ public class Vet extends Person{
             inverseJoinColumns = @JoinColumn(name="speciality_vet"))
     private Set<Speciality> specialties = new HashSet<>();
 
-    public Set<Speciality> getSpecialties() {
-        return specialties;
-    }
 
-    public void setSpecialties(Set<Speciality> specialties) {
-        this.specialties = specialties;
-    }
 }
--- pet-clinic-web/src/main/java/com/gerry/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/gerry/petclinic/bootstrap/DataLoader.java
@@ -64,6 +64,8 @@ public class DataLoader implements CommandLineRunner {
         owner1.setCity("Miami");
         owner1.setTelephone("1231231234");
 
+        Owner x = Owner.builder().address("").firstName("").lastName("").build();
+
         Pet mikesPet = new Pet();
         mikesPet.setPetType(savedDogPetType);
         mikesPet.setOwner(owner1);
