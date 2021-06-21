0d5f414af9ba7e227a6f66baf88e8cfa16235c27
gerry <gerry.baird@uk.ibm.com>
2021-06-21 09:21:31 +0100

    Added JSON Endpoint for Vets

--- pet-clinic-web/src/main/java/com/gerry/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/gerry/petclinic/controllers/VetController.java
@@ -1,10 +1,15 @@
 package com.gerry.petclinic.controllers;
 
 
+import com.gerry.petclinic.model.Vet;
 import com.gerry.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -24,4 +29,10 @@ public class VetController {
         return "vets/index";
 
     }
+
+    @GetMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
+    }
+
 }
